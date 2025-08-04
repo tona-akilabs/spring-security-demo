486a86ddfbffe9872b1b06ebacee1a76c68d34d8
Tona <tona.chheun@akilabs.co>
2025-08-04 14:22:02 +0700

    init

--- src/main/java/com/example/spring_security_demo/web/config/AppSecurityConfig.java
+++ src/main/java/com/example/spring_security_demo/web/config/AppSecurityConfig.java
@@ -1,15 +1,19 @@
 package com.example.spring_security_demo.web.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class AppSecurityConfig {
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     public AppSecurityConfig(PasswordEncoder passwordEncoder) {
         super();
@@ -24,5 +28,15 @@ public class AppSecurityConfig {
                 inMemoryAuthentication().passwordEncoder(passwordEncoder).
                 withUser("user").password(passwordEncoder.encode("pass")).
                 roles("USER");
+    } // @formatter:on
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/delete/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
     } // @formatter:on
 }
